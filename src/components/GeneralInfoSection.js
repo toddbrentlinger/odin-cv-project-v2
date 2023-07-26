0196f6e5b1e8c04689f84aa417d7d69c3cc0908d
toddbrentlinger <toddbrentlinger@gmail.com>
2023-07-26 16:55:06 -0700

    Update default thumbnail image
    Replaces old default thumbnail image url because it had a time limit
    token.

--- src/components/GeneralInfoSection.js
+++ src/components/GeneralInfoSection.js
@@ -9,7 +9,7 @@ function GeneralInfoSection() {
     const [phone, setPhone] = useState("(609) 258-0103");
     const [location, setLocation] = useState("Princeton, New Jersey");
     const [thumbnail, setThumbnail] = useState({
-        url: "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.18172-8/24883563_474515209616195_175625574350703270_o.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=111&ccb=1-7&_nc_sid=8024bb&_nc_ohc=iXYv1CfC8O4AX9wJ59L&_nc_ht=scontent-sjc3-1.xx&oh=00_AfAMPdHJXyHK5IqWa3gIcSESr51SwqZmRMBlOBx__RfKHA&oe=64AECB4C",
+        url: "https://theenchantedmanor.com/wp-content/uploads/2016/06/Professor-Henry-Jones-Jr.jpg",
         size: 110,
         posX: 0,
         posY: 0,
